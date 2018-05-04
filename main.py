eb8c64ca99ca95b16222c709b3c8254d4cf84a61
cjenkins08 <jenkins.chelby@live.com>
2018-05-03 17:47:33 -0700

    new

--- main.py
+++ main.py
@@ -5,53 +5,61 @@ app.config['DEBUG'] = True
 
 form = """
 
-<!Doctype html>
-<html>
-<header>
-    <head>
-        <link type="text/css" rel="stylesheet" href="style.css"/>
-        <title> User Signup Assignment </title>
-    </head>
-</header>
-
-<body>
-  <form action="text", method ='POST'>
-    <label> Username
-        <input name="user" value="0" type="text" />
-    <label>
-    # @todo validate
-    
-    <br>
-    
-    <label> Password
-        <input name="pass1" value="0" type="text" />
-    </label>
-    # @todo validate
-    
-    <br>
-    
-    <label> Verify Password
-        <input name="passver" value="0" type="text" />
-    </label>
-   # @todo validate
-    
-    <br>
-    <label> Email (optional)
-        <input name="email" value="0" type="text" />
-        <input> type = "submit" </input>
-    </label>
-    # @todo validate
-
-</body>
-<footer>
-    C. Jenkins Assignment 3
-</footer>
-</html>
 
 """
 
+
+
 @app.route("/")
 def index():
-    return "form"
+    template = jinja_env.get_template('the_form.html')
+    return template.render()
+
+@app.route("/hello", methods=['POST'])
+def hello():
+    first_name = request.form['first_name']
+    template = jinja_env.get_template9'hello_greeting.html')
+    return template.render(name=firstname)
+
+
+@app.route("/validate-signup")
+def display_form():
+    return the_form.format(email='', username='', password='', verify_password='', 
+    user_error='', password_error='', verify_password_error='', email_error='' )
+
+
+def is_int_or_str(num):
+    try:
+        int(num) or str(num)
+        return True
+    except ValueError:
+        return False
+
+@app.route('/validate-signup', methods=['Post'])
+def validate_form():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.email['email']
+
+    user_error = ''
+    password_error = ''
+    verify_password_error = ''
+
+    if not is_int_or_str(username):
+        user_error = 'Not a valid user'
+
+    if not is_int_or_str(password):
+        password_error = "not a valid password"
+
+    if not is_int_or_str(verify_password):
+        verify_password_error = 'Not a valid verification'
+    if not is_int_or_str(email):
+        email_error = 'Not a valid email'
+
+    else:
+        template = jinja_env.get_template('the_form.html')
+        return template.render(user_error=user)error, password_error=password_error, 
+        verify_password_error=verify_password_error, email_error=email_error, username=username, password=password, email=email)
 
 app.run()
